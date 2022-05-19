a285b80c2e780466e5508687c9ccbb7a83958dd7
maboueita <mahmoud.aboueita@gmail.com>
2022-05-19 20:18:48 +0200

    Apply shape to message body

--- app/src/main/java/com/sample/composedemo/MainActivity.kt
+++ app/src/main/java/com/sample/composedemo/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.sample.composedemo
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -16,69 +13,77 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sample.composedemo.ui.theme.ComposeDemoTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeDemoTheme {
-                MessageCard(Message("Android", "Jetpack Compose"))
+                Column {
+                    Spacer(Modifier.height(20.dp))
+                    MessageCard(message = Message("Mahmoud", "Hello!"))
+                }
             }
         }
     }
 }
 
+
 data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(message: Message) {
-    Row(modifier = Modifier.padding(all = 8.dp)) {
+
+    Row {
         Image(
-            painter = painterResource(R.drawable.ic_launcher_foreground),
-            contentDescription = "Picture",
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "Avatar",
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+                .border(width = 1.5.dp, color = MaterialTheme.colors.secondary, shape = CircleShape)
         )
 
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
             Text(
-                text = message.author,
+                message.author,
                 color = MaterialTheme.colors.secondaryVariant,
                 style = MaterialTheme.typography.subtitle2
             )
-            Spacer(modifier = Modifier.padding(4.dp))
 
-            Surface(shape = MaterialTheme.shapes.small, elevation = 1.dp) {
+            Spacer(modifier = Modifier.width(4.dp))
+
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = Color.Transparent
+            ) {
+
                 Text(
-                    text = message.body,
+                    message.body,
+                    color = MaterialTheme.colors.secondaryVariant,
                     style = MaterialTheme.typography.body2
+
                 )
             }
-
         }
     }
-
 }
-
 
 @Preview
 @Composable
-fun PreviewMessageCard() {
+fun PreviewComposable() {
     ComposeDemoTheme {
-        MessageCard(
-            message = Message(
-                "Colleague",
-                "Hey, take a look at jetpack Compose, it's great!"
-            )
-        )
+        MessageCard(Message("Mahmoud", "Hello World"))
     }
 }
