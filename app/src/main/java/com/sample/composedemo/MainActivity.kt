1f5e513c99a6aec07dbe77f587e304e5d8b659a3
Mahmoud Abou-Eita <mahmoud.aboueita@gmail.com>
2023-03-13 13:46:41 +0200

    Animate conversation list items

--- app/src/main/java/com/sample/composedemo/MainActivity.kt
+++ app/src/main/java/com/sample/composedemo/MainActivity.kt
@@ -1,38 +1,55 @@
 package com.sample.composedemo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.sample.composedemo.ui.theme.ComposeDemoTheme
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeDemoTheme {
-                Column {
-                    Spacer(Modifier.height(20.dp))
-                    MessageCard(message = Message("Mahmoud", "Hello!"))
-                }
-            }
+            previewConversation()
         }
     }
+}
+
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) {
+            MessageCard(it)
+        }
+    }
+}
+
+
+@Composable
+@Preview
+fun previewConversation() {
+    Conversation(SampleData.conversationSample)
 }
 
 
@@ -40,50 +57,60 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(message: Message) {
-
-    Row {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
-            painter = painterResource(id = R.drawable.ic_launcher_foreground),
-            contentDescription = "Avatar",
+            painter = painterResource(R.drawable.ic_launcher_foreground),
+            contentDescription = "Profile Picture",
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
-                .border(width = 1.5.dp, color = MaterialTheme.colors.secondary, shape = CircleShape)
+                .border(1.5.dp, Color.Transparent, CircleShape)
         )
 
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+
+        Column(modifier = Modifier.clickable {
+            isExpanded != isExpanded
+            Log.d("YOLO", "clicked $isExpanded")
+        })
+        {
             Text(
-                message.author,
+                text = message.author,
                 color = MaterialTheme.colors.secondaryVariant,
                 style = MaterialTheme.typography.subtitle2
             )
 
-            Spacer(modifier = Modifier.width(4.dp))
-
+            Spacer(modifier = Modifier.height(4.dp))
 
             Surface(
                 shape = MaterialTheme.shapes.medium,
                 elevation = 1.dp,
-                color = Color.Transparent
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
             ) {
-
                 Text(
-                    message.body,
-                    color = MaterialTheme.colors.secondaryVariant,
-                    style = MaterialTheme.typography.body2
-
+                    text = message.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Integer.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2,
                 )
             }
         }
     }
 }
 
-@Preview
+//@Preview(name = "Light Mode")
+//@Preview(name = "Dark Mode", uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun PreviewComposable() {
-    ComposeDemoTheme {
-        MessageCard(Message("Mahmoud", "Hello World"))
-    }
+    MessageCard(message = Message(author = "Mahmoud", body = "Hello World!"))
 }
